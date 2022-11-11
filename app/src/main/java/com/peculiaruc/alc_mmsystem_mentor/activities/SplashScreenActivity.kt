943c9330c4b48e68fedda2177196c59f28f45b2f
Kenny Dabiri <kennyung6@gmail.com>
2022-11-11 22:48:08 +0100

    Added brand colors, fonts, logo and edited themes.xml file and the SplashScreen

---
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentor/activities/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.peculiaruc.alc_mmsystem_mentor.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.peculiaruc.alc_mmsystem_mentor.MainActivity
+import com.peculiaruc.alc_mmsystem_mentor.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val timeDelay: Long = 3000
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, timeDelay )
+    }
+}
