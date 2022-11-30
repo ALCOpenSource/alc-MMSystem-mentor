4988c0b3aeab739421240e325232c0a94d2a9cba
epicdevler <dev.epicdevler@gmail.com>
2022-11-30 16:05:32 +0100

    Created reusable modal dialog and implemented in EditProfileFragment onDone click

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentor/presentations/components/MMSDialog.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentor/presentations/components/MMSDialog.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.peculiaruc.alc_mmsystem_mentor.R
@@ -11,21 +12,24 @@ import com.peculiaruc.alc_mmsystem_mentor.databinding.MmsDialogLayoutBinding
 
 class MMSDialog(private val context: Context) {
 
-    lateinit var binding: MmsDialogLayoutBinding
-    private val alertDialog = MaterialAlertDialogBuilder(context)
-    private val dialog = alertDialog.create()
+    var binding: MmsDialogLayoutBinding = MmsDialogLayoutBinding.bind(
+        LayoutInflater.from(context).inflate(R.layout.mms_dialog_layout, null, false)
+    )
+    private val alertDialog: MaterialAlertDialogBuilder = MaterialAlertDialogBuilder(context, R.style.MMSDialogTheme)
+    private var dialog: AlertDialog
 
     init {
-        binding = MmsDialogLayoutBinding.bind(
-            LayoutInflater.from(context).inflate(R.layout.mms_dialog_layout, null, false)
-        )
         alertDialog.setView(binding.root)
+
+        dialog = alertDialog.create()
     }
 
     fun show(
-        dialogProperties: DialogProperties
+        dialogProperties: MMSDialogProperties
     ) {
         binding.apply {
+            dialog.setCancelable(dialogProperties.cancelable)
+            dialog.setCanceledOnTouchOutside(dialogProperties.cancelableOnTouchOutside)
             mmsDialogTitle.text = dialogProperties.msg
             dialogProperties.illustration?.let {
                 mmsDialogIllustration.setImageDrawable(
@@ -38,10 +42,12 @@ class MMSDialog(private val context: Context) {
             mmsDialogPosBtn.apply {
                 visibility = visibleIf(dialogProperties.posEnabled)
                 setOnClickListener { dialogProperties.posAction() }
+                text = dialogProperties.posLabel
             }
             mmsDialogNegBtn.apply {
                 visibility = visibleIf(dialogProperties.negEnabled)
                 setOnClickListener { dialogProperties.negAction() }
+                text = dialogProperties.negLabel
             }
         }
 
@@ -51,29 +57,37 @@ class MMSDialog(private val context: Context) {
     private fun visibleIf(condition: Boolean) = if (condition) View.VISIBLE else View.GONE
 
     fun hide() {
-        dialog.hide()
+        dialog.dismiss()
     }
 
-    inner class DialogProperties {
-        var msg: String? = null
+}
 
-        @DrawableRes
-        var illustration: Int? = null
-        var posEnabled: Boolean = false
-        var negEnabled: Boolean = false
-        var posAction: () -> Unit = {}
-        var negAction: () -> Unit = {}
+class MMSDialogProperties {
+    var msg: String? = null
 
-        fun setMsg(msg: String) = apply { this.msg = msg }
-        fun setIllustration(@DrawableRes illustration: Int) =
-            apply { this.illustration = illustration }
+    @DrawableRes
+    var illustration: Int? = null
+    var posEnabled: Boolean = false
+    var posLabel: String = ""
+    var cancelable: Boolean = true
+    var cancelableOnTouchOutside: Boolean = true
+    var negEnabled: Boolean = false
+    var negLabel: String = ""
+    var posAction: () -> Unit = {}
+    var negAction: () -> Unit = {}
 
-        fun setPosEnabled(enabled: Boolean) = apply { this.posEnabled = enabled }
-        fun setNegEnabled(enabled: Boolean) = apply { this.negEnabled = enabled }
-        fun setPosAction(action: () -> Unit) = apply { this.posAction = action }
-        fun setNegAction(action: () -> Unit) = apply { this.negAction = action }
+    fun setMsg(msg: String) = apply { this.msg = msg }
+    fun setIllustration(@DrawableRes illustration: Int) =
+        apply { this.illustration = illustration }
 
+    fun setCancelableOnTouchOutside(cancelable: Boolean) =
+        apply { this.cancelableOnTouchOutside = cancelable }
 
-    }
+    fun setCancelable(cancelable: Boolean) = apply { this.cancelable = cancelable }
+    fun setPosEnabled(enabled: Boolean, label: String) = apply { this.posEnabled = enabled; posLabel = label}
+    fun setNegEnabled(enabled: Boolean, label: String) = apply { this.negEnabled = enabled; negLabel = label }
+    fun setPosAction(action: () -> Unit) = apply { this.posAction = action }
+    fun setNegAction(action: () -> Unit) = apply { this.negAction = action }
+
 
 }
